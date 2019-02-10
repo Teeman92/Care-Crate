99a2835f727e94b4edc13b56b73860de41d16d2c
jpl25 <jpl25@students.uwf.edu>
2019-02-10 15:00:47 -0600

    Layout changes

--- app/src/main/java/com/example/carecrate/LoginActivity.java
+++ app/src/main/java/com/example/carecrate/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.example.carecrate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.emailBtn).setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                //alg.updateHydrationLevel(((TextView)findViewById(R.id.PrintHydration)));
+                Intent intent = new Intent(LoginActivity.this, ChooseCrateActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
